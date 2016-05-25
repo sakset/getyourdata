c4c12a1330ce5f8cda15e8e5d89f2de512e1a881
Matoking <jannepulk@gmail.com>
2016-05-25 15:33:35 +0300

    Add links to front page

--- getyourdata/getyourdata/urls.py
+++ getyourdata/getyourdata/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-
 from home import views as home_views
 
 urlpatterns = [
-	url(r'^organizations/', include('organization.urls')),
+    url(r'^organizations/', include('organization.urls', namespace="organization")),
     url(r'^admin/', admin.site.urls),
 
     url(r'$', home_views.home, name="home"),
