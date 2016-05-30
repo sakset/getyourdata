d0ebe25de67eaff821501c6b27171b89cf14a019
Matoking <jannepulk@gmail.com>
2016-05-30 16:47:55 +0300

    Fix data request tests

--- getyourdata/data_request/tests.py
+++ getyourdata/data_request/tests.py
@@ -22,23 +22,31 @@ class DataRequestCreationTests(TestCase):
             name='other_thing',
             title="Other thing")
 
-        self.assertEquals(len(self.organization.authentication_fields.all()), 0)
+        self.assertEquals(self.organization.authentication_fields.all().count(), 0)
 
         self.organization.authentication_fields.add(self.auth_field1)
         self.organization.authentication_fields.add(self.auth_field2)
 
-        self.assertEquals(len(self.organization.authentication_fields.all()), 2)
+        self.assertEquals(self.organization.authentication_fields.all().count(), 2)
         self.assertEquals(DataRequest.objects.all().count(), 0)
         self.assertEquals(AuthenticationContent.objects.all().count(), 0)
 
+    def test_data_request_form_is_correct(self):
+        response = self.client.get(reverse("data_request:request_data",
+            args=(self.organization.id,)))
+
+        self.assertContains(response, "Some number")
+        self.assertContains(response, "Other thing")
+        self.assertContains(response, "Request your data from Organization")
+
+    def test_data_request_is_created(self):
         self.client.post(
             reverse("data_request:request_data", args=(self.organization.id,)),
-            {"Some number": "1234567",
-             "Other thing": "Some text here"},
+            {"some_number": "1234567",
+             "other_thing": "Some text here"},
             follow=True
             )
 
-    def test_data_request_is_created(self):
         self.assertEquals(DataRequest.objects.all().count(), 1)
 
         data_request = DataRequest.objects.first()
@@ -46,6 +54,13 @@ class DataRequestCreationTests(TestCase):
         self.assertTrue(data_request.organization == self.organization)
 
     def test_authentication_contents_are_created(self):
+        self.client.post(
+            reverse("data_request:request_data", args=(self.organization.id,)),
+            {"some_number": "1234567",
+             "other_thing": "Some text here"},
+            follow=True
+            )
+
         data_request = DataRequest.objects.first()
 
         self.assertTrue(data_request.organization == self.organization)
