c9a08ea1ef839036acf4dd41565d341b2a850505
Matoking <jannepulk@gmail.com>
2016-07-16 12:32:45 +0300

    Clean up admin interface a bit

--- getyourdata/data_request/admin.py
+++ getyourdata/data_request/admin.py
@@ -4,16 +4,6 @@ from data_request.models import DataRequest, AuthenticationContent
 from data_request.models import FaqContent, RequestContent
 
 
-@admin.register(DataRequest)
-class DataRequestAdmin(admin.ModelAdmin):
-    list_display = ['organization', 'created_on']
-
-
-@admin.register(AuthenticationContent)
-class AuthenticationContentAdmin(admin.ModelAdmin):
-    list_display = ['auth_field', 'data_request', 'content', 'created_on']
-
-
 @admin.register(RequestContent)
 class RequestContentAdmin(admin.ModelAdmin):
     list_display = ["title"]
