9ef87b01c077869c6db313c9678ad6874c54c421
Matoking <jannepulk@gmail.com>
2016-08-22 21:38:03 +0300

    Just use a dummy cache for tests and development environment

--- getyourdata/getyourdata/dev_settings.py
+++ getyourdata/getyourdata/dev_settings.py
@@ -17,16 +17,9 @@ DATABASES = {
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
-
-if TESTING:
-    CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-        }
-    }
 
 AUTH_PASSWORD_VALIDATORS = []
 
